371036011bd23bc5a2a1b1331ea8c981ef12476b
Daniel Sungwon Lee <danlee333@gmail.com>
2021-03-09 15:44:12 -0800

    server setup

---
+++ index.js
@@ -0,0 +1,72 @@
+require('dotenv/config');
+const express = require('express');
+const pg = require('pg');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const path = require('path');
+const port = process.env.PORT || 3001;
+
+const path = require('path');
+const express = require('express');
+
+const db = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+const app = express();
+
+//static-middleware
+const publicPath = path.join(__dirname, 'public');
+const staticMiddleware = express.static(publicPath);
+
+app.use(staticMiddleware);
+
+app.use(express.json());
+
+//client-error
+class ClientError {
+  constructor(status, message) {
+    this.status = status;
+    this.message = message;
+  }
+}
+
+//error-middleware
+function errorMiddleware(err, req, res, next) {
+  if (err instanceof ClientError) {
+    res.status(err.status).json({
+      error: err.message
+    });
+  } else {
+    console.error(err);
+    res.status(500).json({
+      error: 'an unexpected error occurred'
+    });
+  }
+}
+
+//API endpoints
+
+
+
+
+//for Heroku deployment
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+
+app.use(errorMiddleware);
+
+app.listen(port, () => {
+  console.log(`express server listening on ${port}`)
+})
