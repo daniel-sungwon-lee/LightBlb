0600e9d41216b3b6af740c0ddd44eea1f1d831f9
Daniel Sungwon Lee <danlee333@gmail.com>
2021-03-10 23:55:21 -0800

    added spinner

--- client/src/auth.jsx
+++ client/src/auth.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { IconButton, TextField, Button } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { IconButton, TextField, Button, CircularProgress } from '@material-ui/core';
 import { MeetingRoomRounded, PersonAddRounded, ArrowBackRounded } from '@material-ui/icons';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -18,9 +18,16 @@ const useStyles = makeStyles({
 
 export default function Auth(props) {
   const classes = useStyles()
+  const [loading, setLoading] = useState(true)
   const [page, setPage] = useState("login")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+
+  useEffect(() => {
+
+   setLoading(false)
+
+  }, [])
 
   const handleChange = (event) => {
     const { id, value } = event.target
@@ -53,57 +60,68 @@ export default function Auth(props) {
 
   }
 
-  if(page === "login") {
+  if(loading) {
     return (
-      <div className="container">
-        <div className="m-5">
-          <img src="/images/lightblb.svg" alt="LightBlb logo" width="200" />
-        </div>
-        <div>
-          <h1 style={{marginTop: "-1.5rem"}}>LightBlb</h1>
-        </div>
-        <form className="d-flex flex-column align-items-center mt-4" onSubmit={handleLogin}>
-          <TextField id="email" label="Email" required InputLabelProps={{required: false}}
-           type="email" value={email} className="mt-4" onChange={handleChange} variant="filled" />
-
-          <TextField id="password" label="Password" required InputLabelProps={{required: false}}
-           type="password" value={password} className="mt-4" onChange={handleChange} variant="filled" />
-
-          <IconButton className="mt-2" type="submit">
-            <MeetingRoomRounded className={classes.icon} />
-          </IconButton>
-        </form>
-        <Button className={classes.button} color="default" startIcon={<PersonAddRounded />}
-          onClick={handleSwitch}>
-          Sign Up
-        </Button>
+      <div className="spinner">
+        <CircularProgress color="inherit" thickness={4} size={50} />
       </div>
     )
-
   } else {
-    return (
-      <div className="container">
-        <div className="m-5">
-          <img src="/images/lightblb.svg" alt="LightBlb logo" width="200" />
-        </div>
-        <div>
-          <h1 style={{ marginTop: "-1.5rem" }}>LightBlb</h1>
-        </div>
-        <form className="d-flex flex-column align-items-center mt-4" onSubmit={handleSignUp}>
-          <TextField id="email" label="Email" required InputLabelProps={{ required: false }}
-            type="email" value={email} className="mt-4" onChange={handleChange} variant="filled" />
 
-          <TextField id="password" label="Password" required InputLabelProps={{ required: false }}
-            type="password" value={password} className="mt-4" onChange={handleChange} variant="filled" />
+    if(page === "login") {
+      return (
+        <div className="container">
+          <div className="m-5">
+            <img src="/images/lightblb.svg" alt="LightBlb logo" width="200" />
+          </div>
+          <div>
+            <h1 style={{marginTop: "-1.5rem"}}>LightBlb</h1>
+          </div>
+          <form className="d-flex flex-column align-items-center mt-4" onSubmit={handleLogin}>
+            <TextField id="email" label="Email" required InputLabelProps={{required: false}}
+             type="email" value={email} className="mt-4" onChange={handleChange} variant="filled" />
 
-          <IconButton className="mt-2" type="submit">
-            <PersonAddRounded className={classes.icon} />
+            <TextField id="password" label="Password" required InputLabelProps={{required: false}}
+             type="password" value={password} className="mt-4" onChange={handleChange} variant="filled" />
+
+            <IconButton className="mt-2" type="submit">
+              <MeetingRoomRounded className={classes.icon} />
+            </IconButton>
+          </form>
+          <Button className={classes.button} color="default" startIcon={<PersonAddRounded />}
+            onClick={handleSwitch}>
+            Sign Up
+          </Button>
+        </div>
+      )
+
+    } else {
+      return (
+        <div className="container">
+          <div className="m-5">
+            <img src="/images/lightblb.svg" alt="LightBlb logo" width="200" />
+          </div>
+          <div>
+            <h1 style={{ marginTop: "-1.5rem" }}>LightBlb</h1>
+          </div>
+          <form className="d-flex flex-column align-items-center mt-4" onSubmit={handleSignUp}>
+            <TextField id="email" label="Email" required InputLabelProps={{ required: false }}
+              type="email" value={email} className="mt-4" onChange={handleChange} variant="filled" />
+
+            <TextField id="password" label="Password" required InputLabelProps={{ required: false }}
+              type="password" value={password} className="mt-4" onChange={handleChange} variant="filled" />
+
+            <IconButton className="mt-2" type="submit">
+              <PersonAddRounded className={classes.icon} />
+            </IconButton>
+          </form>
+          <IconButton onClick={handleSwitch} style={{ marginTop: "2rem" }}>
+            <ArrowBackRounded fontSize="large" />
           </IconButton>
-        </form>
-        <IconButton onClick={handleSwitch} style={{ marginTop: "2rem" }}>
-          <ArrowBackRounded fontSize="large" />
-        </IconButton>
-      </div>
-    )
+        </div>
+      )
+    }
+
   }
+
 }
