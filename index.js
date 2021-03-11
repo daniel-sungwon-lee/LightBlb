1147e54e7d65602ee69f29ec6898596819c00ae5
Daniel Sungwon Lee <danlee333@gmail.com>
2021-03-11 00:12:08 -0800

    added login endpoint

--- index.js
+++ index.js
@@ -67,6 +67,37 @@ app.post('/api/signup', (req, res, next) => {
     .catch(err => next(err));
 });
 
+app.post('/api/login', (req, res, next) => {
+  const { email, password } = req.body;
+
+  const sql = `
+  select "userId", "hashedPassword", "email"
+  from "users"
+  where "email" = $1
+  `;
+  const params = [email];
+
+  db.query(sql, params)
+    .then(result => {
+      const [user] = result.rows;
+      if (!user) {
+        throw new ClientError(401, 'Invalid login');
+      }
+      const { userId, hashedPassword, email } = user;
+      argon2
+        .verify(hashedPassword, password)
+        .then(isMatch => {
+          if (!isMatch) {
+            throw new ClientError(401, 'Invalid login');
+          }
+          const payload = { userId, email };
+          const token = jwt.sign(payload, process.env.TOKEN_SECRET);
+          res.json({ token, user: payload });
+        })
+        .catch(err => next(err));
+    })
+    .catch(err => next(err));
+});
 
 
 //for Heroku deployment
