5243584831223b16817206a007cb1c3026f2ea3c
Daniel Sungwon Lee <danlee333@gmail.com>
2021-03-11 01:34:52 -0800

    WIP

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Auth from './auth';
+import decodeToken from './decode-token';
+import Home from './home';
 
 export default function App() {
   const [user, setUser] = useState(null)
 
   useEffect(() => {
-    if(!user) {
-      return <Redirect to="/auth" />
-    }
-  }, [user])
+    const token = window.localStorage.getItem("userToken")
+    const user = token
+      ? decodeToken(token)
+      : null;
+
+    setUser(user)
+
+  }, [])
 
   const handleLogin = (result) => {
     const { user, token } = result
@@ -18,9 +24,17 @@ export default function App() {
     setUser(user)
     window.localStorage.setItem("userToken", token)
 
-    if(window.localStorage.getItem("userToken")) {
-      return <Redirect to="/" />
+    if (window.localStorage.getItem("userToken")) {
+      <Router>
+        <Redirect to="/" />
+      </Router>
     }
+  }
+
+  if (!user) {
+    <Router>
+      <Redirect to="/auth" />
+    </Router>
   }
 
   return (
@@ -34,7 +48,7 @@ export default function App() {
           </Route>
 
           <Route exact path="/">
-
+            <Home />
           </Route>
 
         </Switch>
