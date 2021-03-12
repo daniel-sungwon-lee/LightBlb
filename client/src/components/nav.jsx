f166e5bdcf6e311dc55146f15babc56fa63f4df3
Daniel Sungwon Lee <danlee333@gmail.com>
2021-03-11 22:34:56 -0800

    nav bug fix

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -36,57 +36,59 @@ export default function Nav(props) {
 
   return (
     <Slide direction="left" in>
-      <AppBar position="sticky" classes={{root: classes.root}} className="sticky-top mb-5">
-        <Toolbar>
-          <IconButton onClick={toggle("left", true)}>
-            <MenuRounded className={classes.icon} />
-          </IconButton>
-          <Drawer anchor={"left"} open={open["left"]} onClose={toggle("left", false)}>
-            <div className={classes.list}>
-              <List className="mt-3">
+      <div className="sticky-top mb-5">
+        <AppBar position="sticky" classes={{root: classes.root}}>
+          <Toolbar>
+            <IconButton onClick={toggle("left", true)}>
+              <MenuRounded className={classes.icon} />
+            </IconButton>
+            <Drawer anchor={"left"} open={open["left"]} onClose={toggle("left", false)}>
+              <div className={classes.list}>
+                <List className="mt-3">
 
-                <Link to="/" className="text-decoration-none">
-                  <Button fullWidth className="p-0">
-                    <ListItem className="d-flex justify-content-center py-3" onClick={toggle("left", false)}>
-                      <h3 className="m-0">
-                        <HomeRounded className={classes.icon} />
-                      </h3>
-                    </ListItem>
-                  </Button>
-                </Link>
-
-                <Link to="/profile" className="text-decoration-none">
-                  <Button fullWidth className="p-0">
-                    <ListItem className="d-flex justify-content-center py-3" onClick={toggle("left", false)}>
-                      <h3 className="m-0">
-                        <PersonRounded className={classes.icon} />
-                      </h3>
-                    </ListItem>
-                  </Button>
-                </Link>
-
-                <Link to="/auth" className="text-decoration-none">
-                  <Tooltip title="Logout" classes={{tooltip: classes.tooltip}}>
-                    <Button fullWidth className="p-0" color="secondary">
-                      <ListItem className="d-flex justify-content-center py-3" onClick={props.handleSignOut}>
-                        <h3 className="m-0" onClick={toggle("left", false)}>
-                          <Icon color="secondary">
-                            <ExitToAppRounded className={classes.iconRed} />
-                          </Icon>
+                  <Link to="/" className="text-decoration-none">
+                    <Button fullWidth className="p-0">
+                      <ListItem className="d-flex justify-content-center py-3" onClick={toggle("left", false)}>
+                        <h3 className="m-0">
+                          <HomeRounded className={classes.icon} />
                         </h3>
                       </ListItem>
                     </Button>
-                  </Tooltip>
-                </Link>
+                  </Link>
 
-              </List>
-            </div>
-            <div className="nav-logo">
-              <img src="images/lightblb.svg" width="60" alt="LightBlb logo" />
-            </div>
-          </Drawer>
-        </Toolbar>
-      </AppBar>
+                  <Link to="/profile" className="text-decoration-none">
+                    <Button fullWidth className="p-0">
+                      <ListItem className="d-flex justify-content-center py-3" onClick={toggle("left", false)}>
+                        <h3 className="m-0">
+                          <PersonRounded className={classes.icon} />
+                        </h3>
+                      </ListItem>
+                    </Button>
+                  </Link>
+
+                  <Link to="/auth" className="text-decoration-none">
+                    <Tooltip title="Logout" classes={{tooltip: classes.tooltip}}>
+                      <Button fullWidth className="p-0" color="secondary">
+                        <ListItem className="d-flex justify-content-center py-3" onClick={props.handleSignOut}>
+                          <h3 className="m-0" onClick={toggle("left", false)}>
+                            <Icon color="secondary">
+                              <ExitToAppRounded className={classes.iconRed} />
+                            </Icon>
+                          </h3>
+                        </ListItem>
+                      </Button>
+                    </Tooltip>
+                  </Link>
+
+                </List>
+              </div>
+              <div className="nav-logo">
+                <img src="images/lightblb.svg" width="60" alt="LightBlb logo" />
+              </div>
+            </Drawer>
+          </Toolbar>
+        </AppBar>
+      </div>
     </Slide>
   )
 }
