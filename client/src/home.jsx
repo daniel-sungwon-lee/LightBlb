790549261491bea620e85aca3417b7ae16877803
Daniel Sungwon Lee <danlee333@gmail.com>
2021-03-13 00:21:14 -0800

    rendered posts

--- client/src/home.jsx
+++ client/src/home.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Card, CardContent, List, ListItem, ListItemAvatar,
+         Avatar, ListItemText } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { FaceRounded } from '@material-ui/icons';
+import Spinner from './components/spinner';
+
+const useStyles = makeStyles({
+  card : {
+    margin: "2rem 1rem"
+  },
+  avatar: {
+    color: "black",
+    backgroundColor: "#FFEC29"
+  }
+})
 
 export default function Home(props) {
+  const classes = useStyles()
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch('/api/posts')
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+        setLoading(false)
+      })
+  }, [])
+
+  if(loading) {
+    return <Spinner />
+  }
+
   return (
     <div className="container">
       <h1>Home</h1>
+      <List>
+        {
+          data.map(post => {
+            const { postId, userId, content } = post
+
+            return (
+              <Card key={postId} className={classes.card}>
+
+                <ListItem>
+                  <CardContent>
+
+                    <ListItemAvatar>
+                      <Avatar classes={{colorDefault: classes.avatar}}>
+                        <FaceRounded />
+                      </Avatar>
+                    </ListItemAvatar>
+
+                    <ListItemText primary={content} secondary={`User ID: ${userId}`} />
+
+
+
+                  </CardContent>
+                </ListItem>
+
+              </Card>
+            )
+          })
+        }
+      </List>
     </div>
   )
 }
