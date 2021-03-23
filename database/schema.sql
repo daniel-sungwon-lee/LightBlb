3aea1201ba00af6b7615caa8328a4808e3b346d9
Daniel Sungwon Lee <danlee333@gmail.com>
2021-03-22 18:34:24 -0700

    updated schema

--- database/schema.sql
+++ database/schema.sql
@@ -25,7 +25,18 @@ CREATE TABLE "saved" (
   OIDS=FALSE
 );
 
+CREATE TABLE "comments" (
+	"commentId" serial NOT NULL,
+	"postId" serial NOT NULL,
+	"userId" serial NOT NULL,
+	"comment" TEXT NOT NULL,
+	CONSTRAINT "comments_pk" PRIMARY KEY ("commentId")
+) WITH (
+  OIDS=FALSE
+);
+
 
 
 ALTER TABLE "posts" ADD CONSTRAINT "posts_fk0" FOREIGN KEY ("userId") REFERENCES "users"("userId");
 ALTER TABLE "saved" ADD CONSTRAINT "saved_fk0" FOREIGN KEY ("postId") REFERENCES "posts"("postId");
+ALTER TABLE "comments" ADD CONSTRAINT "comments_fk0" FOREIGN KEY ("postId") REFERENCES "posts"("postId");
